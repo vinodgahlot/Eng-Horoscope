5a42ff4cd8ae46acce9150584e0554052d2dde4b
Vinod Kumar <vinodonline11@gmail.com>
2020-01-18 16:43:26 +0530

    Update pyhoroscope.py

--- pyhoroscope.py
+++ pyhoroscope.py
@@ -11,19 +11,19 @@ class Horoscope:
     @staticmethod
     def get_todays_horoscope(sunsign):
         url = "http://hindi-horoscope.ictv.in/horoscopes/daily-horoscope/aries/aries.html"
-      
+      return dict
 
     @staticmethod
     def get_weekly_horoscope(sunsign):
         url = "http://hindi-horoscope.ictv.in/horoscopes/daily-horoscope/aries/aries.html"
-        
+        return dict
 
     @staticmethod
     def get_monthly_horoscope(sunsign):
         url = "http://hindi-horoscope.ictv.in/horoscopes/daily-horoscope/aries/aries.html"
-        
+        return dict
 
     @staticmethod
     def get_yearly_horoscope(sunsign):
         url = "http://hindi-horoscope.ictv.in/horoscopes/daily-horoscope/aries/aries.html"
-        
+        return dict
